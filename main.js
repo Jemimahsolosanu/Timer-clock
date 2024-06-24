ab0f7397ef5586b0265d73705715306347575350
Jemimah Oluwalonii <jemimahsolomonsanu@gmail.com>
2024-06-24 13:19:21 +0100

    updated the Ui interface by changing the colors

---
+++ main.js
@@ -0,0 +1,12 @@
+  function updateClock(){
+    let time= document.getElementById("time-clock");
+    let now= new Date()
+
+    hr = String(now.getHours()).padStart(2,"0");
+    min = String(now.getMinutes()).padStart(2,"0");
+    sec = String(now.getSeconds()).padStart(2,"0");
+
+    time.innerHTML=`${hr}:${min}:${sec}`;
+  }
+
+  setInterval(updateClock, 1000);
